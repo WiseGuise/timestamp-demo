74aee039f34cd2e6c8fc997ee29cd32a05499947
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-06-22 15:50:28 -0500

    Changes

--- server.js
+++ server.js
@@ -1,39 +1,64 @@
-const express = require("express");
+// server.js
+// where your node app starts
 
-const cors = require("cors");
-const app = express();
+// init project
+var express = require('express');
+var app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.use(cors({ optionSuccessStatus: 200 }));
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// require statements
 
-app.get("/api/timestamp/:date?", (req, res) => {
-  const date_string = req.params.date_string;
-  let date;
-
-  if (!date_string) {
-    date = new Date();
-  } else {
-    if (!isNaN(date_string)) {
-      date = new Date(parseInt(date_string, 10));
-    } else {
-      date = new Date(date_string);
-    }
-  }
-
-  if (date.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    res.json({ unix: date.getTime() }, { utc: date.toUTCString() });
-  }
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-app.listen(process.env.PORT || 3000,
-	() => console.log("Server is running..."));
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let responseObject = {}
+app.get("/api/timestamp/:date_string", (req, res) => {
+
+  let date_string= req.params.date_string;
+
+  if (date_string.includes("-")) {
+    responseObject["unix"] = new Date(date_string).getTime();
+    responseObject["utc"] = new Date(date_string).toUTCString();
+
+  } else {
+
+    // timestamp
+    date_string= parseInt(date_string);
+    responseObject["unix"] = new Date(date_string).getTime();
+    responseObject["utc"] = new Date(date_string).toUTCString();
+  }
+
+  if (!responseObject["unix"] || !responseObject["utc"]) {
+    res.json({error: "Invalid Date"});
+  }
+
+
+  res.json(responseObject);
+});
+
+app.get("/api/timestamp", (req, res) => {
+  responseObject["unix"] = new Date().getTime();
+  responseObject["utc"] = new Date().toUTCString();
+
+  res.json(responseObject);
+});
