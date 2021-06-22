b021596ef3e6be1c0799eaa6daca7636066980e1
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-06-22 18:45:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,47 +18,41 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-let responseObject = {}
-app.get("/api/timestamp/:date_string", (req, res) => {
-  
-  let date_string= req.params.date_string;
-  
-  if (date_string.includes("-")) {
-    responseObject["unix"] = new Date(date_string).getTime();
-    responseObject["utc"] = new Date(date_string).toUTCString();
-  
+// json container to return timestamp
+let timestamp = {};
+// data analyzing request
+app.get('/api/:date_string', (req, res) => {
+  // states required parameter for processing
+  let date_string = req.params.date_string;
+  // searches for strings including spaces or dashes
+  if (date_string.includes('-') || date_string.includes(' ')) {
+    // returns date_string converted to utc and unix time
+    timestamp['unix'] = new Date(date_string).getTime();
+    timestamp['utc'] = new Date(date_string).toUTCString();
   } else {
-  
-    // timestamp
-    date_string= parseInt(date_string);
-    responseObject["unix"] = new Date(date_string).getTime();
-    responseObject["utc"] = new Date(date_string).toUTCString();
+    // parse date_string into integer format
+    date_string = parseInt(date_string);
+    // convert integer format back into date_string
+    timestamp['unix'] = new Date(date_string).getTime();
+    timestamp['utc'] = new Date(date_string).toUTCString();
+  }
+  // instruction to throw error if not recognized time format
+  if(!timestamp['unix'] || !timestamp['utc']){
+    res.json({error: 'Invalid Date'});
   }
   
-  if (!responseObject["unix"] || !responseObject["utc"]) {
-    res.json({error: "Invalid Date"});
-  }
-  
-    
-  res.json(responseObject);
+  // returns
+  res.json(timestamp);
 });
 
-app.get("/api/timestamp", (req, res) => {
-  responseObject["unix"] = new Date().getTime();
-  responseObject["utc"] = new Date().toUTCString();
-  
-  res.json(responseObject);
+// returns main variables after processing
+app.get('/api/', (req, res) => {
+  timestamp['unix'] = new Date().getTime();
+  timestamp['utc'] = new Date().toUTCString();
+  res.json(timestamp);
 });
