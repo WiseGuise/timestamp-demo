98c80f2bfb8c13249be5a6f43f487b3657499a70
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-06-21 22:16:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 app.use(cors({ optionSuccessStatus: 200 }));
 
