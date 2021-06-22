9bbd67aab46141bdd92374d9da2cf62dcb848c16
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-06-21 22:02:20 -0500

    Changed from demo server to actual server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 app.use(cors({ optionSuccessStatus: 200 }));
 
